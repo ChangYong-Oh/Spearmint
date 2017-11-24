7173254a88c7f28d021f7b22dd3aebe3df7792cb
ChangYong-Oh <changyong.oh0224@gmail.com>
2017-11-24 12:39:56 +0100

    non parallel run and loadavg monitoring

--- run_spearmint.py
+++ run_spearmint.py
@@ -54,7 +54,7 @@ def run_multiple_init(benchmarks_root_dir, exp_list, n_eval_list):
 	run_multiple(dbpath, exp_dir_list, n_eval_list, grid_shift_list)
 
 
-def run_multiple(dbpath, exp_dir_list, n_eval_list, grid_shift_list):
+def run_multiple(dbpath, exp_dir_list, n_eval_list, grid_shift_list, parallel=False):
 	if not os.path.exists(dbpath):
 		os.makedirs(dbpath)
 	mongodb_logfile_name = os.path.join(dbpath, 'mongodb.log')
@@ -64,20 +64,28 @@ def run_multiple(dbpath, exp_dir_list, n_eval_list, grid_shift_list):
 	os.system(cmd_str)
 
 	n_exp = len(exp_dir_list)
-	process_list = []
-	for e in range(n_exp):
-		process = run_spearmint(exp_dir_list[e], n_eval_list[e], grid_shift_list[e])
-		process_list.append(process)
-	n_running = n_exp
-	while n_running > 0:
-		time.sleep(60)
-		running = [elm.poll() is None for elm in process_list]
-		n_running = running.count(True)
-		print('%d/%d is still running %s\ndb path : %s' % (n_running, n_exp, time.strftime("%H:%M:%S"), dbpath))
+	if parallel:
+		process_list = []
 		for e in range(n_exp):
-			if running[e]:
-				print('    %s' % exp_dir_list[e])
-		sys.stdout.flush()
+			process = run_spearmint(exp_dir_list[e], n_eval_list[e], grid_shift_list[e])
+			process_list.append(process)
+		n_running = n_exp
+		while n_running > 0:
+			time.sleep(60)
+			running = [elm.poll() is None for elm in process_list]
+			n_running = running.count(True)
+			print('%d/%d is still running %s\ndb path : %s loadavg : %4.2f, %4.2f, %4.2f' % (n_running, n_exp, time.strftime("%H:%M:%S"), dbpath) + os.getloadavg())
+			for e in range(n_exp):
+				if running[e]:
+					print('    %s' % exp_dir_list[e])
+			sys.stdout.flush()
+	else:
+		for e in range(n_exp):
+			process = run_spearmint(exp_dir_list[e], n_eval_list[e], grid_shift_list[e])
+			while process.poll() is None:
+				time.sleep(60)
+				print('Experiment is running db path : %s exp_dir : %s(%s) loadavg %4.2f, %4.2f, %4.2f' % (dbpath, exp_dir_list[e], time.strftime("%H:%M:%S")) + os.getloadavg())
+				sys.stdout.flush()
 	cmd_str = 'mongod --shutdown --dbpath ' + dbpath
 	if '/var/scratch/' in os.path.realpath(__file__):
 		cmd_str = '/var/scratch/coh/mongodb/mongodb-linux-x86_64-3.4.10/bin/' + cmd_str
